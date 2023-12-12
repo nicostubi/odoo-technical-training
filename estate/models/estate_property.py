2df0e3b850d8552a47d86c38daf680d10a39041c
PEGASE\stubin <nicolas.stubi@romande-energie.ch>
2023-12-12 09:54:30 +0100

    Chapter 6 done

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,20 +2,20 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
-
+from datetime import timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real estate management"
     # _order = "sequence"
 
-    name = fields.Char('Real Estate Name', required=True, translate=True)
+    name = fields.Char('Real Estate Name', default="Unknown", required=True, translate=True)
     description = fields.Text('Description of the asset', required=True)
     postcode = fields.Char('ZIP code')
-    date_availability = fields.Date('Date availability')
+    date_availability = fields.Date('Date availability', default=fields.Datetime.today()+timedelta(days=90), copy=False)
     expected_price = fields.Float('Expected price', required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -25,4 +25,9 @@ class EstateProperty(models.Model):
         string='Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Orientation is used to specify the garden orientation")
+    active = fields.Boolean(default=1)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        required=True, copy=False, default="new")
         
