3164a873015667d04537479356004a8f585ed99e
Nicolas Stübi <nicolas.stubi@gmail.com>
2023-12-13 09:41:43 +0100

    Chapter 9 - added computed field for date_deadline

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from datetime import timedelta
+
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -12,3 +14,16 @@ class EstatePropertyOffer(models.Model):
     ], string='Status', copy=False)
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
+    validity = fields.Integer(string="Validity", default=7)
+    date_deadline = fields.Date(string="Deadline (days)", compute="_compute_date_deadline")
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.validity:
+                if self.create_date:
+                    record.date_deadline = self.create_date + timedelta(days=record.validity)
+                else:
+                    record.date_deadline = fields.Datetime.today() + timedelta(days=record.validity)
+            else:
+                record.date_deadline = None
