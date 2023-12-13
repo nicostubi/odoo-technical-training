fb9afd02589f3067fb042bab338009e5aaa82242
PEGASE\stubin <nicolas.stubi@romande-energie.ch>
2023-12-13 08:56:28 +0100

    chapter 9 - best_price computing done

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -36,8 +36,17 @@ class EstateProperty(models.Model):
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many('estate.property.offer', 'property_id')
     total_area = fields.Integer(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price")
 
     @api.depends('living_area', 'garden_area')
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped('price'))
+            else:
+                record.best_price = None
