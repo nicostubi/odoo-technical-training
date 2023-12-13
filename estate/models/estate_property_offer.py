aebcf422ee78844204bd9dc2d0dca343bb11589e
PEGASE\stubin <nicolas.stubi@romande-energie.ch>
2023-12-13 10:09:45 +0100

    Chapter 9 - Inverse Function done

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -16,9 +16,9 @@ class EstatePropertyOffer(models.Model):
     property_id = fields.Many2one('estate.property', string='Property', required=True)
     validity = fields.Integer(string="Validity (days)", default=7)
     date_deadline = fields.Date(string="Deadline", 
-                                compute="_compute_date_deadline") 
-                                # compute="_compute_date_deadline", 
-                                # inverse="_inverse_date_deadline_from")
+                                #compute="_compute_date_deadline") 
+                                compute="_compute_date_deadline", 
+                                inverse="_inverse_date_deadline_from")
 
     @api.depends('validity')
     def _compute_date_deadline(self):
@@ -32,13 +32,13 @@ class EstatePropertyOffer(models.Model):
             else:
                 record.date_deadline = None
 
-    # def _inverse_date_deadline_from(self):
-    #     for record in self:
-    #         date = record.create_date or fields.Datetime.now()
+    def _inverse_date_deadline_from(self):
+        for record in self:
+            date = record.create_date or fields.Datetime.now()
 
-    #     # If date_deadline is set, calculate validity
-    #     if record.date_deadline:
-    #         delta = record.date_deadline - date.date()
-    #         record.validity = delta.days
-    #     else:
-    #         record.validity = 7  # Or set to a default value
+        # If date_deadline is set, calculate validity
+        if record.date_deadline:
+            delta = record.date_deadline - date.date()
+            record.validity = delta.days
+        else:
+            record.validity = 7  # Or set to a default value
