76c6e8be74a975e273066722d11dc163594951b5
PEGASE\stubin <nicolas.stubi@romande-energie.ch>
2023-12-14 15:39:11 +0100

    Added test for estate property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -95,12 +95,8 @@ class EstateProperty(models.Model):
 
     @api.constrains('selling_price', 'expected_price')
     def _check_selling_price(self):
-        print('test1')
         for record in self:
-            print('record.selling_price : '+ str(record.selling_price))
-            print('float_is_zero(record.selling_price : '+ str(float_is_zero(record.selling_price, 2)))
             if record.selling_price and not float_is_zero(record.selling_price, 2):
-                print('test')
                 min_price = record.expected_price * 0.9
                 if float_compare(record.selling_price, min_price, 2) < 0:
                     raise ValidationError(_('Selling price must be at least 90% of the expected price.'))
